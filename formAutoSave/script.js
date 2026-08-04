7d43f927f6fd31f9d0038e1fb042f8940fc08bd6
Anand <anandvlog03@gmail.com>
2026-08-04 23:26:29 +0530

    add new function code

--- formAutoSave/script.js
+++ formAutoSave/script.js
@@ -1,1 +1,76 @@
-console.log("dfaasd")
+let userForm = document.querySelector("#userForm");
+let clearForm = document.querySelector("#clearForm");
+
+
+let time;
+
+userForm.addEventListener("input", (e) => {
+  clearTimeout(time);
+  time = setTimeout(() => {
+    let genderElement = document.querySelector('input[name="gender"]:checked');
+
+    let userFormsData = {
+      firstName: document.querySelector("#firstName").value,
+      email: document.querySelector("#email").value,
+      phoneNumber: document.querySelector("#phone").value,
+      gender: genderElement ? genderElement.value : "",
+      bio: document.querySelector("#bio").value,
+    };
+
+    localStorage.setItem("userFormsData", JSON.stringify(userFormsData));
+
+     
+  }, 500);
+
+ 
+});
+
+// 2. Restore Data on Page Load
+
+let savedData = localStorage.getItem("userFormsData");
+
+
+// Check localStorage data exists or not
+if (savedData) {
+
+  // Convert string → object
+  let data = JSON.parse(savedData);
+
+
+  // Restore First Name
+  document.querySelector("#firstName").value =
+    data.firstName;
+
+
+  // Restore Email
+  document.querySelector("#email").value =
+    data.email;
+
+
+  // Restore Phone
+  document.querySelector("#phone").value =
+    data.phoneNumber;
+
+
+  // Restore Bio
+  document.querySelector("#bio").value =
+    data.bio;
+
+
+  // Restore Gender
+  if (data.gender) {
+
+    document.querySelector(
+      `input[name="gender"][value="${data.gender}"]`
+    ).checked = true;
+
+  }
+
+  console.log("Data Restored!");
+}
+
+// new user data add kar sakta hai 
+clearForm.addEventListener("click", () => {
+  localStorage.removeItem("userFormsData");
+  userForm.reset();
+});
